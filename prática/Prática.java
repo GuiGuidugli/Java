a06ce6a10a29a46a94f6f451ba1720964a7b14cc
Guilherme Guidugli <105385009+GuiGuidugli@users.noreply.github.com>
2023-02-23 13:13:56 -0300

    Arquivo de prática

--- prática/Prática.java
+++ prática/Prática.java
@@ -4,5 +4,34 @@ package prática;
 
 public class Prática {
     
+public static void main(String[] args) {
+    
+    int op;
+    op = 2;
+
+        switch(op) {
+
+        case 1:
+        System.out.println("case 1: op = " + op);
+        break;
+
+        case 2:
+        System.out.println("case 2: op = " + op);
+        break;
+
+        case 3:
+        System.out.println("case 3: op = " + op);
+        break;
+
+        default:
+
+        System.out.println("default: op não está no limite 1 a 3.");
+
+        break;
+
+        }
+
+    }
+
 }
 
